8b6bacfa0597126e1553a10c5773b65f89849a46
AR.KUDABAYEV <257703403+Kotovsky015@users.noreply.github.com>
2026-01-30 12:06:30 +0500

    Rename Repository and Project

--- Basics/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ Basics/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c6a131e10d8ded3eb407513deffd2195a0589f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
