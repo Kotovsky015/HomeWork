1d39badd8bb37fe1edef0dd23fcc3a140c47e6df
AR.KUDABAYEV <257703403+Kotovsky015@users.noreply.github.com>
2026-02-03 11:29:37 +0500

    Task8

--- ConditionalsAndLoops/Program.cs
+++ ConditionalsAndLoops/Program.cs
@@ -2,13 +2,14 @@
 
 using ConditionalsAndLoops.Task6_CurrencyExchange;
 using ConditionalsAndLoops.Task7_NameOutput;
+using ConditionalsAndLoops.Task8_ProgramWithPassword;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Task7NameOutput task7NameOutput = new Task7NameOutput();
-        task7NameOutput.Run();
+       Task8ProgramWithPassword task8 = new Task8ProgramWithPassword();
+       task8.Run();
        
     }
 }
---
+++ ConditionalsAndLoops/Task8_ProgramWithPassword/Task8ProgramWithPassword.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ConditionalsAndLoops.Task8_ProgramWithPassword;
+
+public class Task8ProgramWithPassword
+{
+    public void Run()
+    {
+        const string Password = "123Test!";
+
+        int maxAttempts = 3;
+        string userInput;
+
+        while (maxAttempts > 0)
+        {
+            Console.Write("Введите пароль:");
+            userInput = Console.ReadLine();
+            if (userInput == Password)
+            {
+                Console.WriteLine("Верный пароль");
+                break;
+            }
+            maxAttempts--;
+            Console.WriteLine($"Не верный пароль. Осталось попыток {maxAttempts}");
+        }
+    }
+}
